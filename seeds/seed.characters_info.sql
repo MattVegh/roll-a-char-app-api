9f81527bf9193242a9a118c7fcabac93795fee46
Matt Vegh <mdv240@gmail.com>
2019-11-25 19:08:26 -0600

    migration and seed

---
+++ seeds/seed.characters_info.sql
@@ -0,0 +1,66 @@
+INSERT INTO characters (
+    fullname, 
+    gender, 
+    race, 
+    class, 
+
+    strength_original,
+    dexterity_original,
+    constitution_original,
+    intelligence_original,
+    wisdom_original,
+    charisma_original,
+
+    strength_race_bonus,
+    charisma_race_bonus,
+
+    strength_total,
+    dexterity_total,
+    constitution_total,
+    intelligence_total,
+    wisdom_total,
+    charisma_total,
+
+    strength_modifier,
+    dexterity_modifier,
+    constitution_modifier,
+    intelligence_modifier,
+    widsom_modifier,
+    charisma_modifier,
+
+    bio,
+    )
+
+    VALUES (
+        'Rhogar Ilbak',
+        'Male',
+        'Dragonborn',
+        'Warlock',
+
+        11,
+        14,
+        14,
+        12,
+        12,
+        15,
+
+        2,
+        1,
+
+        13,
+        14,
+        14,
+        12,
+        12,
+        16,
+
+        '+1',
+        '+2',
+        '+2',
+        '+1',
+        '+1',
+        '+3',
+
+        'An ex-undertaker, Rhogar found a magical relic while burying a body that had a spirit attached to it. Rhogar agreed to a pact with said creature, and was granted magical abilities.'
+
+    )
