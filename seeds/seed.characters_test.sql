c4f6f2a2d28596317389a7918b69f717cbe761e0
Matt Vegh <mdv240@gmail.com>
2019-11-26 20:01:33 -0600

    testing

---
+++ seeds/seed.characters_test.sql
@@ -0,0 +1,60 @@
+INSERT INTO characters (
+    fullname, 
+    gender, 
+    race, 
+    class, 
+
+    strength_original,
+    dexterity_original,
+    constitution_original,
+    intelligence_original,
+    wisdom_original,
+    charisma_original,
+
+    strength_race_bonus,
+    charisma_race_bonus,
+
+    strength_total,
+    dexterity_total,
+    constitution_total,
+    intelligence_total,
+    wisdom_total,
+    charisma_total,
+
+    strength_modifier,
+    dexterity_modifier,
+    constitution_modifier,
+    intelligence_modifier,
+    widsom_modifier,
+    charisma_modifier,
+
+    bio
+    )
+
+    VALUES (
+        'Rock',
+        'Paper-plane',
+        'Tree',
+        'Scissors',
+        11,
+        12,
+        14,
+        15,
+        11,
+        14,
+        3,
+        3,
+        14,
+        12,
+        14,
+        15,
+        11,
+        17,
+        '+2',
+        '+1',
+        '+2',
+        '+2',
+        '0',
+        '+3',
+        'Snip snip'
+    )
