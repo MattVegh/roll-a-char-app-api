1447802e616da17ace38ff349c10f06fa9b869d4
Matt Vegh <mdv240@gmail.com>
2019-12-03 22:18:40 -0600

    removed redunancies

--- test/characters.fixtures.js
+++ test/characters.fixtures.js
@@ -16,26 +16,12 @@ function makeCharactersArray() {
             wisdom_original: 11,
             charisma_original: 14,
 
-            strength_race_bonus: 3,
-            charisma_race_bonus: 3,
-            dexterity_race_bonus: null,
-            constitution_race_bonus: null,
-            intelligence_race_bonus: null,
-            wisdom_race_bonus: null,
-
             strength_total: 14,
             dexterity_total: 12,
             constitution_total: 14,
             intelligence_total: 15,
             wisdom_total: 11,
             charisma_total: 17,
-
-            strength_modifier: '+2',
-            dexterity_modifier: '+1',
-            constitution_modifier: '+2',
-            intelligence_modifier: '+2',
-            widsom_modifier: '0',
-            charisma_modifier: '+3',
 
             bio: 'Snip snip'
         }
@@ -62,22 +48,12 @@ module.exports = {
 //     "wisdom_original" : "11",
 //     "charisma_original" : "14",
 
-//     "strength_race_bonus" : "3",
-//     "charisma_race_bonus" : "3",
-
 //     "strength_total" : "14",
 //     "dexterity_total" : "12",
 //     "constitution_total" : "14",
 //     "intelligence_total" : "15",
 //     "wisdom_total" : "11",
 //     "charisma_total" : "17",
-
-//     "strength_modifier" : "+2",
-//     "dexterity_modifier" : "+1",
-//     "constitution_modifier" : "+2",
-//     "intelligence_modifier" : "+2",
-//     "widsom_modifier" : "0",
-//     "charisma_modifier" : "+3",
 
 //     "bio": "Snip snip"
 // }
