e21ccd2e0e8af63bc368f2df721005523b417b01
Matt Vegh <mdv240@gmail.com>
2019-12-03 22:28:06 -0600

    1

--- src/characters/characters-router.js
+++ src/characters/characters-router.js
@@ -4,6 +4,46 @@ const CharacterService = require('./characters-service')
 const jsonParser = express.json()
 const characterRouter = express.Router()
 
+
+// const serializedCharacter = character => ({
+//     id: 1,
+//     date_created: new Date(),
+//     fullname: 'Rock',
+//     gender: 'Paper-plane',
+//     race: 'Tree',
+//     classType: 'Scissors',
+
+//     strength_original: 11,
+//     dexterity_original: 12,
+//     constitution_original: 14,
+//     intelligence_original: 15,
+//     wisdom_original: 11,
+//     charisma_original: 14,
+
+//     strength_race_bonus: 3,
+//     charisma_race_bonus: 3,
+//     dexterity_race_bonus: null,
+//     constitution_race_bonus: null,
+//     intelligence_race_bonus: null,
+//     wisdom_race_bonus: null,
+
+//     strength_total: 14,
+//     dexterity_total: 12,
+//     constitution_total: 14,
+//     intelligence_total: 15,
+//     wisdom_total: 11,
+//     charisma_total: 17,
+
+//     strength_modifier: '+2',
+//     dexterity_modifier: '+1',
+//     constitution_modifier: '+2',
+//     intelligence_modifier: '+2',
+//     widsom_modifier: '0',
+//     charisma_modifier: '+3',
+
+//     bio: 'Snip snip'
+// })
+
 characterRouter
     .route('/')
     .get((req, res, next) => {
@@ -34,7 +74,12 @@ characterRouter
             intelligence_total,
             wisdom_total,
             charisma_total,
-
+            strength_modifier,
+            dexterity_modifier,
+            constitution_modifier,
+            intelligence_modifier,
+            widsom_modifier,
+            charisma_modifier,
             bio,
         } = req.body
 
@@ -49,14 +94,24 @@ characterRouter
             intelligence_original,
             wisdom_original,
             charisma_original,
-            
+            strength_race_bonus,
+            charisma_race_bonus,
+            dexterity_race_bonus,
+            constitution_race_bonus,
+            intelligence_race_bonus,
+            wisdom_race_bonus,
             strength_total,
             dexterity_total,
             constitution_total,
             intelligence_total,
             wisdom_total,
             charisma_total,
-            
+            strength_modifier,
+            dexterity_modifier,
+            constitution_modifier,
+            intelligence_modifier,
+            widsom_modifier,
+            charisma_modifier,
             bio,
         }
 
@@ -70,7 +125,7 @@ characterRouter
                     
                     .json(character)
             })
-            .catch((e) => console.log(e), next)
+            .catch(next)
     })
 
 
