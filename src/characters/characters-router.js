f85056ccaeb33d06b607edbcd3fca7ea05073e4c
Matt Vegh <mdv240@gmail.com>
2019-12-07 12:44:27 -0600

    reverting changes

--- src/characters/characters-router.js
+++ src/characters/characters-router.js
@@ -21,17 +21,13 @@ characterRouter
             gender,
             race,
             class_type,
-
             strength_original,
             dexterity_original,
             constitution_original,
             intelligence_original,
             wisdom_original,
             charisma_original,
-
-            strength_race_bonus,
-            charisma_race_bonus,
-
+        
             strength_total,
             dexterity_total,
             constitution_total,
@@ -39,14 +35,7 @@ characterRouter
             wisdom_total,
             charisma_total,
 
-            strength_modifier,
-            dexterity_modifier,
-            constitution_modifier,
-            intelligence_modifier,
-            widsom_modifier,
-            charisma_modifier,
-
-            bio
+            bio,
         } = req.body
 
         const newCharacter = {
@@ -54,17 +43,13 @@ characterRouter
             gender,
             race,
             class_type,
-
             strength_original,
             dexterity_original,
             constitution_original,
             intelligence_original,
             wisdom_original,
             charisma_original,
-
-            strength_race_bonus,
-            charisma_race_bonus,
-
+            
             strength_total,
             dexterity_total,
             constitution_total,
@@ -72,25 +57,17 @@ characterRouter
             wisdom_total,
             charisma_total,
             
-            strength_modifier,
-            dexterity_modifier,
-            constitution_modifier,
-            intelligence_modifier,
-            widsom_modifier,
-            charisma_modifier,
-
-            bio
+            bio,
         }
 
         CharacterService.insertCharacter(
             req.app.get('db'),
             newCharacter
-            
         )
             .then(character => {
-                console.log('newchar is', newCharacter)
                 res
                     .status(201)
+                    
                     .json(character)
             })
             .catch(next)
